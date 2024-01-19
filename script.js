330d412cf60ff70d112c08f68f9c940df4d75516
Yordani <yalvarezb93@gmail.com>
2024-01-19 21:08:28 +0300

    All functional

--- script.js
+++ script.js
@@ -4,21 +4,50 @@ const containerTop = document.querySelector(".container-top");
 const containerUnder = document.querySelector(".container-under");
 const rating = document.querySelectorAll(".rating button");
 
+const ratingOne = document.getElementById("one");
+const ratingTwo = document.getElementById("two");
+const ratingThree = document.getElementById("three");
+const ratingFour = document.getElementById("four");
+const ratingFive = document.getElementById("five");
+
+// Changing card    
 btnSubmit.addEventListener("click", (e) => {
     e.stopPropagation();
     containerTop.classList.add("hidden");
     containerUnder.classList.remove("hidden");
 });
-
 document.addEventListener('click', () => {
     containerTop.classList.remove("hidden");
     containerUnder.classList.add("hidden");
 });
 
-rating.forEach((btn) => {
-btn.addEventListener("click", () => {
-    btn.classList.add("active");
-    
+// Rating state start
+
+rating.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.forEach((rate) => {
+            rate.classList.remove("active");
+        });
+        rate.classList.add("active");
+    }); 
 })
 
+ratingOne.addEventListener("click", () => {
+    document.querySelector('span').innerHTML = '1';
+})
+
+ratingTwo.addEventListener("click", () => {
+    document.querySelector('span').innerHTML = '2';
+})
+
+ratingThree.addEventListener("click", () => {
+    document.querySelector('span').innerHTML = '3';
+})
+
+ratingFour.addEventListener("click", () => {
+    document.querySelector('span').innerHTML = '4';
+})
+
+ratingFive.addEventListener("click", () => {
+    document.querySelector('span').innerHTML = '5';
 })
