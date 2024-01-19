4d0b5ce1ad7ed0169dd8c90ed754129052a28955
Yordani <yalvarezb93@gmail.com>
2024-01-19 20:23:37 +0300

    Add classes hover and focus to the css

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-const btnSubmit = document.querySelector(".Submit-button");
-const containerTop =   document.querySelector(".container-top")
-const containerUnder = document.querySelector(".container-under")
 
-btnSubmit.addEventListener("click", () => {
+const btnSubmit = document.querySelector(".Submit-button");
+const containerTop = document.querySelector(".container-top");
+const containerUnder = document.querySelector(".container-under");
+const rating = document.querySelectorAll(".rating button");
+
+btnSubmit.addEventListener("click", (e) => {
+    e.stopPropagation();
     containerTop.classList.add("hidden");
     containerUnder.classList.remove("hidden");
-})
-
+});
 
 document.addEventListener('click', () => {
+    containerTop.classList.remove("hidden");
+    containerUnder.classList.add("hidden");
+});
+
+rating.forEach((btn) => {
+btn.addEventListener("click", () => {
+    btn.classList.add("active");
     
- 
+})
+
 })
