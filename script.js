16c50071ac0f81e364beefb15650a6ccdd13a29d
Yordani <yalvarezb93@gmail.com>
2024-01-20 13:25:37 +0300

    Optimazed Javascript

--- script.js
+++ script.js
@@ -2,52 +2,72 @@
 const btnSubmit = document.querySelector(".Submit-button");
 const containerTop = document.querySelector(".container-top");
 const containerUnder = document.querySelector(".container-under");
+
 const rating = document.querySelectorAll(".rating button");
 
-const ratingOne = document.getElementById("one");
-const ratingTwo = document.getElementById("two");
-const ratingThree = document.getElementById("three");
-const ratingFour = document.getElementById("four");
-const ratingFive = document.getElementById("five");
+const onBack =()=>{
+ containerTop.classList.remove("hidden");
+        containerUnder.classList.add("hidden");
+}
+    
+const handleClickOutside = (e) => {
+    if (e.target.tagName === "BODY") onBack();
+}
+
+
+document.addEventListener('keyup', (e) => {
+    if (e.key === 'Escape' || e.key == 'ArrowLeft') {
+        onBack()
+    }
+})
+document.querySelector('body').addEventListener('click', (e) => {
+    handleClickOutside(e)
+    // onBack()
+})
 
 // Changing card    
 btnSubmit.addEventListener("click", (e) => {
     e.stopPropagation();
+    
+    const activeRate = document.querySelectorAll("button.active");
+    if (activeRate.length == 0) return alert("Please select a rating");
+    
     containerTop.classList.add("hidden");
     containerUnder.classList.remove("hidden");
 });
-document.addEventListener('click', () => {
-    containerTop.classList.remove("hidden");
-    containerUnder.classList.add("hidden");
-});
 
-// Rating state start
 
-rating.forEach((rate) => {
+const writeOnSpan = (number) => {
+    localStorage.setItem("rating-active", number);
+    document.querySelector('span').innerHTML = number;
+}
+
+const setRatingInactive = () => {
+    rating.forEach((rate) => {
+        rate.classList.remove("active");
+    });
+}
+
+const loadPreviousRating = () => {
+    return localStorage.getItem("rating-active");
+}
+
+rating.forEach((rate, index) => {
+    const activeRating = loadPreviousRating()
+    if(activeRating)  rating[activeRating -1].classList.add('active');
+
     rate.addEventListener("click", () => {
-        rating.forEach((rate) => {
-            rate.classList.remove("active");
-        });
+        setRatingInactive()
+        writeOnSpan(index + 1)
         rate.classList.add("active");
-    }); 
+    });
 })
 
-ratingOne.addEventListener("click", () => {
-    document.querySelector('span').innerHTML = '1';
-})
 
-ratingTwo.addEventListener("click", () => {
-    document.querySelector('span').innerHTML = '2';
-})
 
-ratingThree.addEventListener("click", () => {
-    document.querySelector('span').innerHTML = '3';
-})
+// document.addEventListener('keyup', (e) => { 
+//     console.log(e)
+// })
 
-ratingFour.addEventListener("click", () => {
-    document.querySelector('span').innerHTML = '4';
-})
-
-ratingFive.addEventListener("click", () => {
-    document.querySelector('span').innerHTML = '5';
-})
+// document.addEventListener('click', (e) => { console.log(e) })
+// document.addEventListener('scroll', (e) => { console.log(e) })
